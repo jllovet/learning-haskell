b2c3f03c15daee4cfd1f19af5216d8366da64c57
Jonathan Llovet <jonathan.llovet@gmail.com>
2020-04-24 23:50:53 -0400

    Add clarification of recursive-imperative function equivalence

--- haskell-fundamentals-part1/hello-world/005-loopAsRecursiveFunction.hs
+++ haskell-fundamentals-part1/hello-world/005-loopAsRecursiveFunction.hs
@@ -8,8 +8,12 @@
 --     return x;
 -- }
 -- This can be rewritten as a recursive function or set of functions
+
+-- The recursive case here uses a helper function pow2loop
 pow2 n = pow2loop n 1 0
 pow2loop n x i =
     if i < n
+        -- Here, the pow2loop function is called again with "updated"
+        -- versions of the x and i values
         then pow2loop n (x*2) (i+1)
         else x
