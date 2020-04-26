f4e16f052dd93aac0ef04834cf46d105ae283d3d
Jonathan Llovet <jonathan.llovet@gmail.com>
2020-04-26 14:53:33 -0400

    Add derek-banas-haskell-tutorial

---
+++ derek-banas-haskell-tutorial/haskell-tutorial.hs
@@ -0,0 +1,26 @@
+{-
+
+******************************
+Getting started
+******************************
+
+At a terminal, type ghci to open a Haskell REPL
+:l <haskell-file> will load in a Haskell file to your session
+
+For example:
+ghci
+:l haskell-tutorial
+
+:r will run everything in the file that is currently loaded
+
+******************************
+Comment Syntax
+******************************
+
+-- Single line comments are started with double dashes
+
+{- 
+Multiline comments use curly braces and dashes
+-}
+
+-}
