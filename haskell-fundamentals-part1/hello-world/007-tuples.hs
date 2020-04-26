a9d650cf2b4aaa5225fec50c99e90dbc3ae71e80
Jonathan Llovet <jonathan.llovet@gmail.com>
2020-04-26 01:26:23 -0400

    Add notes on common functions used with tuples

--- haskell-fundamentals-part1/hello-world/007-tuples.hs
+++ haskell-fundamentals-part1/hello-world/007-tuples.hs
@@ -8,3 +8,22 @@ x = (1, "hello")
 y = ("pi", 3.14159, [1,2,3], "four")
 
 -- However, a list has dynamic length, while a tuple has a fixed length
+
+-- Tuples are often used to effectively return multiple values from a function
+headAndLength list = (head list, length list)
+
+-- These functions access elements of the tuple
+-- fst - accesses the first element
+-- snd - accesses the second element
+
+-- a = (1, "hello")
+
+-- fst a
+-- returns: 1
+
+-- snd a
+-- returns: "hello"
+
+-- If tuples are more than about 3 elements long, the code becomes fragile
+-- and difficult to maintain. Instead of long tuples, custom data types are
+-- recommended.
