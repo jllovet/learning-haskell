f92a57fd7c9b21b9aba7c8a73c74f0bec7e95350
Jonathan Llovet <jonathan.llovet@gmail.com>
2020-04-26 01:17:52 -0400

    Add high level tuple and list comparison

---
+++ haskell-fundamentals-part1/hello-world/007-tuples.hs
@@ -0,0 +1,10 @@
+-- Tuples can contain elements of multiple types, while lists cannot.
+-- Example list:
+list = [1,2,3,4]
+-- Example tuple:
+x = (1, "hello")
+
+-- Another example tuple with several types
+y = ("pi", 3.14159, [1,2,3], "four")
+
+-- However, a list has dynamic length, while a tuple has a fixed length
