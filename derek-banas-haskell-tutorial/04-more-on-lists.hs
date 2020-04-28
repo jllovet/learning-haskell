89bceb6947c6508fc23abdf26e6063b852f78da8
Jonathan Llovet <jonathan.llovet@gmail.com>
2020-04-27 23:08:29 -0400

    Add examples of built in list functions

--- derek-banas-haskell-tutorial/04-more-on-lists.hs
+++ derek-banas-haskell-tutorial/04-more-on-lists.hs
@@ -26,3 +26,29 @@ isListEmpty = null morePrimes2 -- will return False
 -- To access specific indices, <list> !! <index>
 
 fourthItem = morePrimes2 !! 3
+
+firstPrime = head morePrimes2
+
+lastPrime = last morePrimes2
+
+initPrimes = init morePrimes2 -- returns all but last element of list
+
+first3Primes = take 3 morePrimes2
+
+removedPrimes = drop 3 morePrimes2 -- returns list after the n elements specified
+
+letters = take 5 (drop 10 ['a'..'z'])
+-- letters 
+-- "klmno"
+
+is7ElementOfList = 7 `elem` morePrimes2
+
+alternativeIs7ElementOfList = elem 7 morePrimes2
+
+maxPrime = max morePrimes2
+
+minPrime = min morePrimes2
+
+mySum = sum [1,2,3,4,5] -- 15
+
+myProduct = product [1,2,3,4,5] -- 120
