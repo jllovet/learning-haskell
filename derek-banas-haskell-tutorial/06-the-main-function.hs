1e49d5f72a16a1a85859e0a1ab8583f16bd4310a
Jonathan Llovet <jonathan.llovet@gmail.com>
2020-05-01 22:56:19 -0400

    Add instructions for compiling and running a Haskell script

--- derek-banas-haskell-tutorial/06-the-main-function.hs
+++ derek-banas-haskell-tutorial/06-the-main-function.hs
@@ -1,6 +1,14 @@
 import Data.List
 import System.IO
 
+{-
+To compile this file, run the following:
+ghc --make 06-the-main-function.hs
+
+run it:
+./06-the-main-function
+-}
+
 main = do
     putStrLn "What's your name?"
     -- store console input in name
